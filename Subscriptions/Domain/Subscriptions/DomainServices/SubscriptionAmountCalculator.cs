ea7b4693f2a4eedff3ab3f1b6b8c2d9b4875046d
Bdruid <fadidib23@hotmail.com>
2022-10-24 22:02:51 +0300

    ShouldReturnSubscriptionAmountWithTwentyPercentDiscount -> FAIL

---
+++ Subscriptions/Domain/Subscriptions/DomainServices/SubscriptionAmountCalculator.cs
@@ -0,0 +1,19 @@
+﻿using Subscriptions.Domain.Abstractions.ISubscriptions;
+using Subscriptions.Domain.Base;
+using Subscriptions.Domain.Customers;
+using Subscriptions.Domain.Products;
+
+namespace Subscriptions.Domain.Subscriptions.DomainServices
+{
+    public sealed class SubscriptionAmountCalculator : ISubscriptionAmountCalculator
+    {
+        public decimal Calculate(Customer customer, Product product)
+        {
+            var amount = Factory.Get<IDiscount>(x => x.IsValid(customer.MoneySpent));
+
+            var subscriptionAmount = amount.Price(product.Amount.value);
+
+            return subscriptionAmount;
+        }
+    }
+}
