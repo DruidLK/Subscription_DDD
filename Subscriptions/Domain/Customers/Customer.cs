9bd190d0b3f4ebc045fee57d830e4d80a0c377b0
Bdruid <fadidib23@hotmail.com>
2022-10-24 22:18:09 +0300

    Refactor

--- Subscriptions/Domain/Customers/Customer.cs
+++ Subscriptions/Domain/Customers/Customer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Subscriptions.Domain.Base;
+using Subscriptions.Domain.Products;
 using Subscriptions.Domain.Subscriptions;
 using Subscriptions.Domain.ValueObjects;
 
@@ -19,12 +20,30 @@ namespace Subscriptions.Domain.Customers
         public IReadOnlyCollection<Subscription> Subscriptions =>
             this.subscriptions.AsReadOnly();
 
-        public Customer(Email Email, CustomerName CustomerName, decimal MoneySpent)
+        public Customer(Email Email, CustomerName CustomerName)
             : this()
         {
             Id = Guid.NewGuid();
             this.Email = Email ?? throw new ArgumentNullException(nameof(Email));
             this.CustomerName = CustomerName ?? throw new ArgumentNullException(nameof(CustomerName));
+        }
+
+        public void AddSubscription(
+            Product product,
+            Customer customer,
+            decimal amount,
+            DateTimeOffset currentPeriodEndDate)
+        {
+            var subscription = new Subscription(
+                product.Id,
+                customer.Id,
+                this,
+                product,
+                amount,
+                currentPeriodEndDate);
+
+            this.subscriptions.Add(subscription);
+            this.MoneySpent += subscription.Amount;
         }
     }
 }
