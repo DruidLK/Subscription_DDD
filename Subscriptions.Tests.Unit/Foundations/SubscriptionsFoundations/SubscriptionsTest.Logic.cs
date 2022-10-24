20578422696cf2448a7139e5d92f4714045d1d72
Bdruid <fadidib23@hotmail.com>
2022-10-24 21:44:27 +0300

    ShouldReturnOriginalSubscriptionAmount -> PASS

---
+++ Subscriptions.Tests.Unit/Foundations/SubscriptionsFoundations/SubscriptionsTest.Logic.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Subscriptions.Domain.Customers;
+using Subscriptions.Domain.Products;
+using Subscriptions.Domain.ValueObjects;
+using Xunit;
+
+namespace Subscriptions.Tests.Unit.Foundations.SubscriptionsFoundations
+{
+    public partial class SubscriptionsTest
+    {
+        [Fact]
+        public void ShouldReturnOriginalSubscriptionAmount()
+        {
+            // Arrange - Given
+            const int Fifteen = 15;
+
+            var customer =
+                new Customer(new Email("email"), new CustomerName("f","d"), MoneySpent: 99);
+
+            var product =
+                new Product("f", new Money(15), BillingPeriod.Weekly);
+
+            var expectedAmount = Fifteen;
+
+            // Act - When
+            var actualAmount =
+                this.subscriptionAmountCalculator
+                    .Calculate(customer, product);
+
+            // Assert - Then
+            actualAmount.Should().Be(expectedAmount);
+        }
+    }
+}
---
+++ Subscriptions.Tests.Unit/Foundations/SubscriptionsFoundations/SubscriptionsTest.cs
@@ -0,0 +1,12 @@
+﻿using Subscriptions.Domain.Subscriptions.DomainServices;
+
+namespace Subscriptions.Tests.Unit.Foundations.SubscriptionsFoundations
+{
+    public partial class SubscriptionsTest
+    {
+        private readonly SubscriptionAmountCalculator subscriptionAmountCalculator;
+
+        public SubscriptionsTest() =>
+            this.subscriptionAmountCalculator = new SubscriptionAmountCalculator();
+    }
+}
