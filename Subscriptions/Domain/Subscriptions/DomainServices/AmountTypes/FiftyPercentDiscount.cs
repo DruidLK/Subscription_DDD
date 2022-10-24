d62f88404fc1b348871e0af46519b24bbe931cf1
Bdruid <fadidib23@hotmail.com>
2022-10-24 23:24:22 +0300

    ShouldReturnSubscriptionAmountWithFiftyPercentDiscount -> PASS

---
+++ Subscriptions/Domain/Subscriptions/DomainServices/AmountTypes/FiftyPercentDiscount.cs
@@ -0,0 +1,14 @@
+﻿namespace Subscriptions.Domain.Subscriptions.DomainServices.AmountTypes
+{
+    public sealed class FiftyPercentDiscount : IDiscount
+    {
+        const int OneThousand = 1000;
+        const decimal FiftyPercent = 0.5M;
+
+        public bool IsValid(decimal amount) =>
+            amount >= OneThousand;
+
+        public decimal Price(decimal money) =>
+            money * FiftyPercent;
+    }
+}
---
+++ Subscriptions/Domain/Subscriptions/DomainServices/AmountTypes/TwentyPercentDiscount.cs
@@ -0,0 +1,13 @@
+﻿namespace Subscriptions.Domain.Subscriptions.DomainServices.AmountTypes
+{
+    public sealed class TwentyPercentDiscount : IDiscount
+    {
+        const int OneHundred = 100;
+        const decimal EightyPercent = 0.8M;
+        public bool IsValid(decimal amount) =>
+            amount is OneHundred or < 1000;
+
+        public decimal Price(decimal money) =>
+            money * EightyPercent;
+    }
+}
