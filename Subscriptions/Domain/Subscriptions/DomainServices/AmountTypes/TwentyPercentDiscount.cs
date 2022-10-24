55944a006add62618224ebcdf86220cee22ab3fc
Bdruid <fadidib23@hotmail.com>
2022-10-24 23:27:03 +0300

    Refactor

--- Subscriptions/Domain/Subscriptions/DomainServices/AmountTypes/TwentyPercentDiscount.cs
+++ Subscriptions/Domain/Subscriptions/DomainServices/AmountTypes/TwentyPercentDiscount.cs
@@ -3,9 +3,11 @@
     public sealed class TwentyPercentDiscount : IDiscount
     {
         const int OneHundred = 100;
+        const int Thousand = 1000;
+
         const decimal EightyPercent = 0.8M;
         public bool IsValid(decimal amount) =>
-            amount is OneHundred or < 1000;
+            amount is >= OneHundred and < Thousand;
 
         public decimal Price(decimal money) =>
             money * EightyPercent;
